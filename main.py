8bced350b5529031ebd470d5b5479e1732440ad1
SambiTheKing11 <sambandhsingh777@gmail.com>
2024-01-07 21:24:02 -0600

    Created a window using tkinter library

---
+++ main.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+'''
+            Log Jan 7, 2023
+    Just starting using tkinter, cheers to a successful project! :)
+'''
+#
+root = tk.Tk()
+
+# Set window title
+root.title("My Tkinter Window")
+
+# Set window size
+root.geometry("300x300")
+
+# Start the GUI event loop
+root.mainloop()
